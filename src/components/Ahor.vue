6150a278aaa454caa80f019406d0afdb05792877
Nicoll2104 <soda210461@gmail.com>
2023-08-29 14:14:31 -0500

    primero

---
+++ src/components/Ahor.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="body">
+      <button v-for="(item,index) in alfa" :key="index" >{{ item }}</button>
+    </div>
+  </template>
+  
+  <script setup>
+  const alfa =[ "A","B","C","D","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+  </script>
+  
+  <style scoped>
+  
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Ahor from './components/Ahor.vue'
 
 createApp(App).mount('#app')
+createApp(Ahor).mount('#ahor')
